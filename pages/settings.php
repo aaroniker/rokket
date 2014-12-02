a24e105fb852ba67ec42854a3520c62cbe217cb3
callofsorrow <info@grenzless.de>
2014-12-02 22:32:04 +0100

    page: settings

--- pages/settings.php
+++ pages/settings.php
@@ -28,13 +28,112 @@
 			echo message::success(lang::get('generate_css_success'));	
 	}
 	
+	if(isset($_POST['send'])) {
+		
+		rp::add('lang', type::post('lang', 'string'), true);
+		rp::add('logs', type::post('logs', 'int'), true);
+		rp::add('ip', type::post('ip', 'string'), true);
+		rp::add('email', type::post('email', 'string'), true);
+		rp::add('emailNot', type::post('emailNot', 'int'), true);
+		
+		rp::save();
+			
+		echo message::success(lang::get('settings_edited'));	
+	}
+	
 ?>
 
 <div class="row">
 
-	<div class="col-md-7">
+    <form action="" method="post">
+	
+    <div class="col-md-7">
+    
+    	<div class="panel">
+        	<div class="top">
+            	<h3><?=lang::get('general'); ?></h3>
+            </div>
+            <div class="content">
+            
+                <div class="input row">
+                    <label class="col-sm-4"><?=lang::get('language'); ?></label>
+                    <div class="col-sm-8">
+                        
+                        <select name="lang">
+                        <option value="<?=rp::get('lang'); ?>"><?=lang::get('lang_select'); ?></option>
+                        <?php
+                            $handle = opendir(dir::base('lib'.DIRECTORY_SEPARATOR.'lang'.DIRECTORY_SEPARATOR));
+                            
+                            while($file = readdir($handle)) {
+                            
+                                if(in_array($file, ['.', '..']))
+                                    continue;
+                            
+                                echo '<option value="'.$file.'">'.$file.'</option>';
+                            }
+                        ?>
+                        </select>
+                    </div>
+                    
+                </div>
+            
+                <div class="input row">
+                    <label class="col-sm-4"><?=lang::get('logs'); ?></label>
+                    <div class="col-sm-8">
+    
+                        <div class="switch">
+                            <input name="logs" id="logs" value="1" type="checkbox" <?=(rp::get('logs')) ? 'checked="checked"' : ''; ?>>
+                            <label for="logs"></label>
+                            <div><?=lang::get('yes'); ?></div>
+                        </div>
+                        
+                    </div>
+                </div>
+            
+                <div class="input row">
+                    <label class="col-sm-4"><?=lang::get('ip'); ?></label>
+                    <div class="col-sm-8">
+                        <input type="text" name="ip" value="<?=rp::get('ip'); ?>">
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+        <div class="panel">
+        	<div class="top">
+            	<h3><?=lang::get('notifications'); ?></h3>
+            </div>
+            <div class="content">
+            
+                <div class="input row">
+                    <label class="col-sm-4"><?=lang::get('email_notifcation'); ?></label>
+                    <div class="col-sm-8">
+    
+                        <div class="switch">
+                            <input name="emailNot" id="emailNot" value="1" type="checkbox" <?=(rp::get('emailNot')) ? 'checked="checked"' : ''; ?>>
+                            <label for="emailNot"></label>
+                            <div><?=lang::get('yes'); ?></div>
+                        </div>
+                        
+                    </div>
+                </div>
+            
+                <div class="input row">
+                    <label class="col-sm-4"><?=lang::get('email'); ?></label>
+                    <div class="col-sm-8">
+                        <input type="text" name="email" value="<?=rp::get('email'); ?>">
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+        <button type="submit" name="send"><?=lang::get('apply'); ?></button>
     
     </div>
+                
+    </form>
 	
     <div class="col-md-5">
     	
