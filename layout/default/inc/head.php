c7a10a51c3f80159ff2134896e360384a266f5b0
callofsorrow <info@grenzless.de>
2014-12-02 22:28:50 +0100

    add favicon

--- layout/default/inc/head.php
+++ layout/default/inc/head.php
@@ -6,6 +6,9 @@
 
 <title><?=layout::getPage(); ?> - Rokket Webinterface</title>
 
+<link rel="shortcut icon" href="layout/default/img/favicon.png" type="image/png" />
+<link rel="icon" href="layout/default/img/favicon.png" type="image/png" />
+
 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:400,300">
 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700">
 <link rel="stylesheet" href="/layout/default/css/animate.css">
