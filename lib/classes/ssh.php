7688f12db6910a134786bc036ba4910271265eaa
callofsorrow <info@grenzless.de>
2014-12-04 00:21:44 +0100

    edit ssh class not static

--- lib/classes/ssh.php
+++ lib/classes/ssh.php
@@ -2,28 +2,36 @@
 
 class ssh {
 	
-	static $host;
-	static $user;
-	static $password;
+	protected $host;
+	protected $user;
+	protected $pass;
 	
-	static $ssh;
+	protected $ssh;
 	
-	static public function connect($host, $user, $password) {
+	public function __construct($host, $user, $pass) {
 	
-		self::$host = $host;
-		self::$user = $user;
-		self::$password = $password;
+		$this->host = $host;
+		$this->user = $user;
+		$this->password = $pass;
 			
 		set_include_path(get_include_path().PATH_SEPARATOR.'lib/vendor/phpseclib');
 		include('lib/vendor/phpseclib/Net/SSH2.php');
 	
-		self::$ssh = new Net_SSH2($host);
+		$this->ssh = new Net_SSH2($host);
 		
-		if (!self::$ssh->login($user, $password)) {
-		    echo message::danger(lang::get('ssh_login_failed'));
+		if (!$this->ssh->login($user, $pass)) {
+		    echo message::danger(lang::get('ssh_login_failed'), false);
 			return false;
 		}
+		
+		return $this->ssh;
 	
+	}
+	
+	public function exec($cmd) {
+		
+		return $this->ssh->exec($cmd);
+		
 	}
 	
 }
