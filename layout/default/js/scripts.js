b221ff7486022f7cf4bb1de893efd6c0bb2d8c4e
callofsorrow <info@grenzless.de>
2014-12-08 20:48:47 +0100

    update scripts & add ajax for server cfg

--- layout/default/js/scripts.js
+++ layout/default/js/scripts.js
@@ -1,48 +1,56 @@
 $(document).ready(function() {
 	
-	$('select').each(function(){
-		var $this = $(this), numberOfOptions = $(this).children('option').length;
-	  
-		$this.addClass('select-hidden'); 
-		$this.wrap('<div class="select"></div>');
-		$this.after('<div class="select-styled"></div>');
+	function setSelect() {
 	
-		var $styledSelect = $this.next('div.select-styled');
-		$styledSelect.text($this.children('option').eq(0).text());
-	  
-		var $list = $('<ul />', {
-			'class': 'select-options'
-		}).insertAfter($styledSelect);
-	  
-		for (var i = 0; i < numberOfOptions; i++) {
-			$('<li />', {
-				text: $this.children('option').eq(i).text(),
-				rel: $this.children('option').eq(i).val()
-			}).appendTo($list);
-		}
-	  
-		var $listItems = $list.children('li');
-	  
-		$styledSelect.click(function(e) {
-			e.stopPropagation();
-			$('div.select-styled.active').each(function(){
-				$(this).removeClass('active').next('ul.select-options').hide();
-			});
-			$(this).toggleClass('active').next('ul.select-options').toggle();
+		$('select').each(function(){
+			var $this = $(this), numberOfOptions = $(this).children('option').length;
+		  	if(!$this.hasClass('select-hidden')) {
+				$this.addClass('select-hidden'); 
+				$this.wrap('<div class="select"></div>');
+				$this.after('<div class="select-styled"></div>');
+			
+				var $styledSelect = $this.next('div.select-styled');
+				$styledSelect.text($this.children('option').eq(0).text());
+			  
+				var $list = $('<ul />', {
+					'class': 'select-options'
+				}).insertAfter($styledSelect);
+			  
+				for (var i = 0; i < numberOfOptions; i++) {
+					$('<li />', {
+						text: $this.children('option').eq(i).text(),
+						rel: $this.children('option').eq(i).val()
+					}).appendTo($list);
+				}
+			  
+				var $listItems = $list.children('li');
+			  
+				$styledSelect.click(function(e) {
+					e.stopPropagation();
+					$('div.select-styled.active').each(function(){
+						$(this).removeClass('active').next('ul.select-options').hide();
+					});
+					$(this).toggleClass('active').next('ul.select-options').toggle();
+				});
+			  
+				$listItems.click(function(e) {
+					e.stopPropagation();
+					$styledSelect.text($(this).text()).removeClass('active');
+					$this.val($(this).attr('rel'));
+					$list.hide();
+					$this.trigger('change');
+				});
+			  
+				$(document).click(function() {
+					$styledSelect.removeClass('active');
+					$list.hide();
+				});
+			}
 		});
-	  
-		$listItems.click(function(e) {
-			e.stopPropagation();
-			$styledSelect.text($(this).text()).removeClass('active');
-			$this.val($(this).attr('rel'));
-			$list.hide();
-		});
-	  
-		$(document).click(function() {
-			$styledSelect.removeClass('active');
-			$list.hide();
-		});
-	});
+	
+	}
+	
+	setSelect();
 	
 	$('.circle').each(function(){
 	
@@ -201,6 +209,22 @@ $(document).ready(function() {
         	result[type] = elements
 		});
 		$("#inputRights").val(JSON.stringify(result));
+	});
+	
+	$("select[name='gameID']").change(function() {
+		
+		var gameID = $(this).val();
+		
+		$.ajax({
+			url: "index.php",
+			type: "POST",
+			data: { page: "server", action: "add", id : gameID },
+			success: function(data) {
+				$("#ajax").slideUp(200).html(data).slideDown(600);
+				setSelect();
+			}
+		});
+			
 	});
 	
 });
