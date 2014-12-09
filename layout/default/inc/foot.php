b473a6483df6b3add64538766587d17b921b8438
callofsorrow <info@grenzless.de>
2014-12-09 01:33:50 +0100

    delete dev less.js

--- layout/default/inc/foot.php
+++ layout/default/inc/foot.php
@@ -3,27 +3,7 @@
     <script src="http://cdnjs.cloudflare.com/ajax/libs/snap.svg/0.3.0/snap.svg-min.js"></script>
 	<script>
 		var polyfilter_scriptpath = '/';
-		/*less = {
-    		env: "development"
-		};
-		function destroyLessCache(pathToCss) {
- 
-			if (!window.localStorage || !less || less.env !== 'development') {
-				return;
-			}
-			var host = window.location.host;
-			var protocol = window.location.protocol;
-			var keyPrefix = protocol + '//' + host + pathToCss;
-			for (var key in window.localStorage) {
-				if (key.indexOf(keyPrefix) === 0) {
-					delete window.localStorage[key];
-				}
-			}
-		}
-		destroyLessCache('/layout/default/less/');
-		localStorage.clear();*/
 	</script>
-	<!--<script src="/layout/default/js/less.js"></script>-->
     <script src="/layout/default/js/modal.js"></script>
 	<script src="/layout/default/js/scripts.js"></script>
 </body>
