da898f374b903c42ea9ee986ec269c47e48cd0c6
callofsorrow <info@grenzless.de>
2014-12-09 02:51:31 +0100

    update modal plugin

--- layout/default/inc/foot.php
+++ layout/default/inc/foot.php
@@ -1,9 +1,7 @@
 	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/snap.svg/0.3.0/snap.svg-min.js"></script>
-	<script>
-		var polyfilter_scriptpath = '/';
-	</script>
+	<script src="https://cdn.polyfill.io/v1/polyfill.min.js"></script>
     <script src="/layout/default/js/modal.js"></script>
 	<script src="/layout/default/js/scripts.js"></script>
 </body>
