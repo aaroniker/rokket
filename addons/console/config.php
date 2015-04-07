260a3aea703e4c8362378330ca1f68f29c88a5b9
callofsorrow <info@grenzless.de>
2015-04-07 19:58:42 +0200

    add console addon

---
+++ addons/console/config.php
@@ -0,0 +1,7 @@
+<?php
+	
+	layout::addNav(lang::get('console'), 'console', 'terminal', [], true);
+	
+	layout::addCSS('addons/console/layout/css/console.css');
+	layout::addJS('addons/console/layout/js/console.js');
+?>
