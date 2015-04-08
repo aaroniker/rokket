8d32b6e275aafe6f14116a8a934b2fe64c4990d2
callofsorrow <info@grenzless.de>
2015-04-08 06:16:38 +0200

    add ajax call

--- addons/console/layout/js/console.js
+++ addons/console/layout/js/console.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 	
+	function ajaxd() { 
+		$.ajax({
+			type: "POST",
+			url: "index.php?page=console",
+			data: "",
+			success: function(data) {
+				$("#terminal").append(data);
+				ajaxd();
+			}
+		});
+	}
+	ajaxd();
+
 });
