a38783a82e9cc00516ab7a78f8a6649bb97caa73
callofsorrow <info@grenzless.de>
2015-04-08 06:16:29 +0200

    remove temp

--- pages/server.php
+++ pages/server.php
@@ -209,22 +209,6 @@
 		
 	}
 	
-	$SSH = rp::get('SSH');
-		
-	$host = $SSH['ip'];
-	$user = $SSH['user'];
-	$pass = $SSH['password'];
-	
-	unset($SSH);
-	
-	$ssh = new ssh($host, $user, $pass);
-	
-	echo nl2br($ssh->read('/.*@.*[$|#]/', NET_SSH2_READ_REGEX));
-	
-	$ssh->write("ping google.de \n\r");
-	$ssh->setTimeout(3);
-	
-	echo nl2br($ssh->read('/.*@.*[$|#]/', NET_SSH2_READ_REGEX));
 ?>
 
 <div class="row">
