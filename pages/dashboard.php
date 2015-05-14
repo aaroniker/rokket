6a4e1775385fb4d99690457dbf3a491aec1fba86
callofsorrow <info@grenzless.de>
2015-05-14 05:10:00 +0200

    dashboard update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -9,88 +9,113 @@
 ?>
 
 <div class="row">
-	
-    <div class="col-md-3 col-sm-6">
-    	<div class="stats">
-            <?php
-            	$ram = $host->ram();
-			?>
-        	<div class="text">
-            	<h3>RAM</h3>
-                <?=sprintf(lang::get('ram_total'), $ram['total'].' MB'); ?>
+
+    <div class="col-lg-4">
+    
+    	<h2><?=lang::get('rokket_about'); ?></h2>
+        
+        <p><?=lang::get('rokket_text'); ?></p>
+    	
+        <div class="row">
+        	<div class="col-sm-6">
+        		<a href="https://www.facebook.com/pages/Rokket/1562909277254179" class="button facebook full svg" target="_blank"><?=layout::svg('facebook'); ?>Facebook</a>
+        	</div>
+        	<div class="col-sm-6">
+        		<a href="https://github.com/callofsorrow/rokket" class="button github full svg" target="_blank"><?=layout::svg('github'); ?>GitHub</a>
+        	</div>
+        </div>
+    
+    </div>
+    
+    <div class="col-lg-8">
+    
+        <div class="row">
+            
+            <div class="col-md-6">
+                <div class="stats">
+                    <?php
+                        $ram = $host->ram();
+                    ?>
+                    <div class="text">
+                        <h3>RAM</h3>
+                        <?=sprintf(lang::get('ram_total'), $ram['total'].' MB'); ?>
+                    </div>
+                    
+                    <div class="circle" data-color="2f323a">
+                        <div><?=$ram['percentage']; ?>%</div>
+                        <svg data-color="f5f6f9"></svg>
+                    </div>
+                
+                </div>
             </div>
             
-            <div class="circle" data-color="31343B">
-        	    <div><?=$ram['percentage']; ?>%</div>
-        	    <svg data-color="f5f6f9"></svg>
-        	</div>
-        
-        </div>
-    </div>
-	
-    <div class="col-md-3 col-sm-6">
-    	<div class="stats primary">
-            <?php
-            	$cpu = $host->cpu();
-			?>
-        	<div class="text">
-            	<h3>CPU</h3>
-                <?=sprintf(lang::get('cpu_average'), $cpu['num']); ?>
+            <div class="col-md-6">
+                <div class="stats primary">
+                    <?php
+                        $cpu = $host->cpu();
+                    ?>
+                    <div class="text">
+                        <h3>CPU</h3>
+                        <?=sprintf(lang::get('cpu_average'), $cpu['num']); ?>
+                    </div>
+                    
+                    <div class="circle" data-color="da6765">
+                        <div><?=$cpu['load']; ?>%</div>
+                        <svg data-color="ffffff"></svg>
+                    </div>
+                
+                </div>
             </div>
             
-            <div class="circle" data-color="da6765">
-        	    <div><?=$cpu['load']; ?>%</div>
-        	    <svg data-color="ffffff"></svg>
-        	</div>
-        
-        </div>
-    </div>
-	
-    <div class="col-md-3 col-sm-6">
-    	<div class="stats">
-            <?php
-            	$hdd = $host->hdd();
-				$numHdd = count($hdd);
-				
-				$percentHdd = 0;
-				$total = 0;
-				
-				foreach($hdd as $disk) {
-					$percentHdd = $percentHdd + $disk['percentage'];
-					$total = $total + toByte($disk['total']);
-				}
-				$average = $percentHdd / $numHdd;
-			?>
-        	<div class="text">
-            	<h3>HDD</h3>
-                <?=sprintf(lang::get('hdd_avail'), byteToSize($total)); ?>
+            <div class="col-md-6">
+                <div class="stats">
+                    <?php
+                        $hdd = $host->hdd();
+                        $numHdd = count($hdd);
+                        
+                        $percentHdd = 0;
+                        $total = 0;
+                        
+                        foreach($hdd as $disk) {
+                            $percentHdd = $percentHdd + $disk['percentage'];
+                            $total = $total + toByte($disk['total']);
+                        }
+                        $average = $percentHdd / $numHdd;
+                    ?>
+                    <div class="text">
+                        <h3>HDD</h3>
+                        <?=sprintf(lang::get('hdd_avail'), byteToSize($total)); ?>
+                    </div>
+                    
+                    <div class="circle" data-color="2f323a">
+                        <div><?=$average; ?>%</div>
+                        <svg data-color="f5f6f9"></svg>
+                    </div>
+                
+                </div>
             </div>
             
-            <div class="circle" data-color="31343B">
-        	    <div><?=$average; ?>%</div>
-        	    <svg data-color="f5f6f9"></svg>
-        	</div>
-        
-        </div>
-    </div>
-	
-    <div class="col-md-3 col-sm-6">
-    	<div class="stats">
-        	<div class="text">
-            	<h3>UPTIME</h3>
-                <?=lang::get('since_start'); ?>
+            <div class="col-md-6">
+                <div class="stats">
+                    <div class="text">
+                        <h3>UPTIME</h3>
+                        <?=lang::get('since_start'); ?>
+                    </div>
+                    <div class="right">
+                        <?=$host->uptime(true); ?>
+                    </div>
+                </div>
             </div>
-            <div class="right">
-            	<?=$host->uptime(true); ?>
-            </div>
+            
         </div>
+    
     </div>
     
 </div>
 
 <div class="row">
 
-    <div class="col-md-8">
+    <div class="col-lg-8 col-md-7">
     
         <div class="panel">
             <div class="top">
@@ -134,15 +159,26 @@
     
     </div>
     
-    <div class="col-md-4">
+    <div class="col-lg-4 col-md-5">
     
-    	<h2><?=lang::get('rokket_about'); ?></h2>
-        
-        <p><?=lang::get('rokket_text'); ?></p>
+    	<h2><?=lang::get('latest_blog'); ?></h2>
     
-        <a href="https://www.facebook.com/pages/Rokket/1562909277254179" class="button facebook full svg" target="_blank"><?=layout::svg('facebook'); ?>Facebook</a>
-        <a href="https://github.com/callofsorrow/rokket" class="button github full svg" target="_blank"><?=layout::svg('github'); ?>GitHub</a>
-        <a href="http://rokket.info" class="button none full" target="_blank"><?=lang::get('visit_website'); ?></a>
+        <div class="timeline">
+            <div class="item">
+            	<div class="circle"></div>
+            	<div class="text">
+            		<p><a href="">We have a timeline item</a></p>
+                    <small>25.0.4.2015</small>
+            	</div>
+        	</div>
+            <div class="item">
+            	<div class="circle"></div>
+            	<div class="text">
+            		<p><a href="">We have a timeline item</a></p>
+                    <small>25.0.4.2015</small>
+            	</div>
+        	</div>
+        </div>
     
     </div>
 
